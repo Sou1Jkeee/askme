b6c01f8ca485e96a7951d5effa657231c6630ca9
Sou1Jkeee <1nfern0@list.ru>
2020-04-08 10:55:27 +0300

    fixed sessions and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root "users#index"
 
-  resources :users
-  resources :sessions, only: %i[new create destroy]
+  resources :users,     except: %i[destroy]
+  resource  :session,   only:   %i[new create destroy]
   resources :questions, except: %i[show new index]
 
   # get 'users/index'
@@ -11,8 +11,4 @@ Rails.application.routes.draw do
   # get 'users/edit'
   # get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
 end
