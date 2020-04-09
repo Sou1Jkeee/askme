73b2e78858f4c79f607339c45cb4cb2f06db1784
Sou1Jkeee <1nfern0@list.ru>
2020-04-09 13:57:40 +0300

    non register users can add questions, create_question migration fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,22 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', optional: true
 
   validates :text, :user, presence: true
   validates :text, length: { maximum: 255 }
-
-#  before_validation :before_validation
-#  after_validation :after_validation
-
-#  before_save :before_save
-#  after_save :after_save
-
-#  before_create :before_create
-#  after_create :after_create
-
-#  before_update :before_update
-#  after_update :after_update
-
-#  before_destroy :before_destroy
-#  after_destroy :after_destroy
 end
--- db/migrate/20200303140927_create_questions.rb
+++ db/migrate/20200303140927_create_questions.rb
@@ -1,5 +1,4 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
-  before_validation :normalize_username_and_email
   def change
     create_table :questions do |t|
       t.string :text
